6ad650413de8d59c8c3284aed7e995baef4e8168
Ayaz Mehmood <154249172+Sh00la@users.noreply.github.com>
2024-01-25 20:16:30 +0500

    Update app.js

--- app.js
+++ app.js
@@ -9,19 +9,21 @@ console.log (b+c/a)
 
 alert("Hello to the Page")
 confirm("Do you want to proceed?")
-var inp = prompt("Enter the Number")
-console.log(inp)
-document.write(inp + "<br>")
-document.write(`${inp} x 1 = `, inp*1 + "<br>")
-document.write(`${inp} x 2 = `, inp*2 + "<br>")
-document.write(`${inp} x 3 = `, inp*3 + "<br>")
-document.write(`${inp} x 4 = `, inp*4 + "<br>")
-document.write(`${inp} x 5 = `, inp*5 + "<br>")
-document.write(`${inp} x 6 = `, inp*6 + "<br>")
-document.write(`${inp} x 7 = `, inp*7 + "<br>")
-document.write(`${inp} x 8 = `, inp*8 + "<br>")
-document.write(`${inp} x 9 = `, inp*9 + "<br>")
-document.write(`${inp} x 10 = `, inp*10 + "<br>")
+var startNumber = prompt("Enter the number for tables");
+var endNumber = 20;
+
+startNumber = parseInt(startNumber);
+
+for (var i = startNumber; i <= endNumber; i++) {
+    document.write(`<h3>Multiples of ${i}:</h3>`);
+    
+    for (var j = 1; j <= 10; j++) {
+        document.write(`${i} x ${j} = ${i * j}<br>`);
+    }
+
+    document.write("<br>");
+}
+
 
 const ptr = prompt("Tell me Your Percentage")
 
@@ -29,12 +31,16 @@ if (ptr<= 50) {
     console.log("Work hard for better result")
 } else if(ptr<= 60){
     console.log("Grade C")
+    console.log("Mehnat ki zarurat hy bachy")
 }else if(ptr<= 70){
     console.log("Grade B")
+    console.log("Boht khoob. Ap is sy behter perform kr skty ho")
 }else if(ptr<= 80){
     console.log("Grade A")
+    console.log("Ary wah. Tum ny to kamaal kr dia. wese thori si mehnat sy boht uper ja skty ho")
 }else if(ptr<= 100){
     console.log("Grade A-1")
+    console.log("O teri ki. Saary number tum ny hi ly liye kya? boht khoob")
 }else{
     console.log("Please Input the correct data")
 }
